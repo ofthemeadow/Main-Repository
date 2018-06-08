e11a05ea347fc0d968572f78c00ec7e15358bfbe
ofthemeadow <37228366+ofthemeadow@users.noreply.github.com>
2018-06-07 18:14:53 -0700

    new new

--- C#/CensusSurveyExc/CensusSurveyExc/Program.cs
+++ C#/CensusSurveyExc/CensusSurveyExc/Program.cs
@@ -8,6 +8,16 @@ namespace ConsoleApp5
         {
             Console.WriteLine("Welcome to the U.S. Census Survey");
             Console.WriteLine("What is your age?");
+                int age = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("How many hours of television do you watch per day?");
+                float hoursOfTv = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("What is your annual salary?");
+                decimal annualSalary = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Are you married? Please anser 'true' or 'false'.");
+                bool isMarried = Convert.ToBoolean(Console.ReadLine());
+            Console.WriteLine("What is your favourite type of donut ?");
+                string favouriteDonut = Convert.ToString(Console.ReadKey());
+            Console.WriteLine("Thank you for your answers! Have a good day.");
             Console.ReadLine();
         }
     }
