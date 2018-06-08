b2f60ebca620df821625e99c21af003b25a9dfaa
ofthemeadow <37228366+ofthemeadow@users.noreply.github.com>
2018-06-07 17:27:29 -0700

    updates

---
+++ C#/welcome/welcome/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace ConsoleApp3
+{
+    class welcome
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Acme Accounting Systems");
+            Console.WriteLine("Remember, we're 'accounting' on you!");
+            Console.ReadLine();
+        }
+    }
+}
