444da75a87b681aaf4bb667d53de38f4074fd82c
ofthemeadow <37228366+ofthemeadow@users.noreply.github.com>
2018-06-07 17:11:23 -0700

    update

---
+++ C#/basicprogramexc.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace ConsoleApp5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to the U.S. Census Survey");
+            Console.WriteLine("What is your age?");
+            Console.ReadLine();
+        }
+    }
+}
