c10a64e8b2f676916e533fac992c0274378ae9d0
ofthemeadow <37228366+ofthemeadow@users.noreply.github.com>
2018-06-11 22:05:15 -0700

    arrays

--- C#/arraystringsintegers/arraystringsintegers/Program.cs
+++ C#/arraystringsintegers/arraystringsintegers/Program.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace building_exercise
+namespace arraystringsintegers
 {
     class Program
     {
--- C#/arraystringsintegers/arraystringsintegers/Properties/AssemblyInfo.cs
+++ C#/arraystringsintegers/arraystringsintegers/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("building exercise")]
+[assembly: AssemblyTitle("arraystringsintegers")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("building exercise")]
+[assembly: AssemblyProduct("arraystringsintegers")]
 [assembly: AssemblyCopyright("Copyright ©  2018")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("8aa8a73a-ba78-4931-9832-381a8a1f7afa")]
+[assembly: Guid("a6dd2684-4571-4c1c-88bd-bf8d97b78b19")]
 
 // Version information for an assembly consists of the following four values:
 //
