cc8d9d1b1b7391230764898027896e9870194345
ofthemeadow <37228366+ofthemeadow@users.noreply.github.com>
2018-06-11 20:49:21 -0700

    string exercise

---
+++ C#/stringbuilderexercise/stringbuilderexercise/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace stringbuilderexercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string lName = "del Prado ";
+            string fName = "Steffan ";
+            string quote = "My name is ";
+            string concatenate = quote + fName + lName;
+
+            concatenate = concatenate.ToUpper();
+
+            StringBuilder sb = new StringBuilder();
+            sb.Append("I have lived in PDX for five years now. ");
+            sb.Append("\n \t PDX is my new home. ");
+            sb.Append("\nThank you.");
+
+            Console.WriteLine(concatenate);
+            Console.WriteLine(sb);
+            Console.Read();
+        }
+    }
+}
