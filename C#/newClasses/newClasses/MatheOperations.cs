29c3cbf33074f2400cff2a82df8aa0ac4c25e75f
ofthemeadow <37228366+ofthemeadow@users.noreply.github.com>
2018-06-28 16:19:27 -0700

    classes

---
+++ C#/newClasses/newClasses/MatheOperations.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace newClasses
+{
+    class MatheOperations
+    {
+        public static void Add(int num1, int num2)
+        {
+            Console.WriteLine(num1 + num2);
+        }
+        public static void Multiply(int num1, int num2)
+        {
+            Console.WriteLine(num1 * num2);
+        }
+        public static void Sub(int num1, int num2)
+        {
+            Console.WriteLine(num1 - num2);
+        }
+    }
+}
---
+++ C#/newClasses/newClasses/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace newClasses
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+            Console.WriteLine("Give me a whole number to add to 31.");
+            MatheOperations.Add(Convert.ToInt32(Console.ReadLine()), 31); 
+
+            Console.WriteLine("Give me a whole number and I'll subtract 28 from it.");
+            MatheOperations.Sub(Convert.ToInt32(Console.ReadLine()), 28);
+
+            Console.WriteLine("Give me a whole number and I'll multiply it by 23.");
+            MatheOperations.Multiply(Convert.ToInt32(Console.ReadLine()), 23);
+            }
+            catch (FormatException ex)
+            {
+                Console.WriteLine("That decimal is not going to work.");
+                Console.ReadLine();
+            }
+        }
+    }
+}
