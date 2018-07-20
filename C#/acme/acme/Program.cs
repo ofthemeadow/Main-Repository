d4f1e4d26991c5e4854a1ff25892c672ed2ed70a
ofthemeadow <37228366+ofthemeadow@users.noreply.github.com>
2018-07-20 13:34:47 -0700

    exercise 34

---
+++ C#/acme/acme/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace acme
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Acme Accoundting Systems");
+            Console.WriteLine("Remember, we're \"accounting\" on you!");
+            Console.ReadLine();
+        }
+    }
+}
