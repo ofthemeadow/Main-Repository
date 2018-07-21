8dd9b2c4390509a4d33feadbcd186d8a55bba728
ofthemeadow <37228366+ofthemeadow@users.noreply.github.com>
2018-07-20 22:41:38 -0700

    bool
    booleon expressions

---
+++ C#/dui/dui/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace dui
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("What is your age?");
+            int age = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Have you ever had a DUI? \t\nType: yes or no");
+            string DUI = Console.ReadLine();
+
+            Console.WriteLine("How many speeding tickets do you have?");
+            int tickets = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Qualified?");
+            bool qualified = (age > 15 && DUI == "no" && tickets <= 3);
+            Console.WriteLine(qualified);
+            Console.Read();
+        }
+    }
+}
