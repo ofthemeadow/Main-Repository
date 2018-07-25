6f707089772ab334340eedbd3f3a889fad4c39bd
ofthemeadow <37228366+ofthemeadow@users.noreply.github.com>
2018-07-24 20:36:23 -0700

    arrays and lists

---
+++ C#/ArraysAndLists/ArraysAndLists/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ArraysAndLists
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Select an index number, \n\tbetween 0 and 5, \nI will gather and display the string at that string.");
+            string[] spArray = new string[] { "one", "two", "three", "four", "five", "six" };
+            int userNum = Convert.ToInt32(Console.ReadLine());
+            if (userNum >= 5)
+                Console.WriteLine("You have selected an index number that does not exist.");
+            Console.WriteLine(spArray[userNum] + " Press ENTER.");
+            Console.ReadLine();
+
+            Console.WriteLine("Select an index number, \n\tbetween 0 and 5, \nI will gather and display the integer at that integer.");
+            int[] numArray = new int[] { 47, 987, 56, 777, 23, 365 };
+            int userNum2 = Convert.ToInt32(Console.ReadLine());
+            if (userNum2 >= 5)
+                Console.WriteLine("You have selected an index number that does not exist.");
+            Console.WriteLine(numArray[userNum2] + " Press ENTER.");
+            Console.ReadLine();
+
+            Console.WriteLine("Select an index number, \n\tbetween 0 and 5, \nI will gather and display it from my List.");
+            List<string> mixArray = new List<string> { "I", "love", "my", "dog", "Curious", "Hendricks" };
+            int userNum3 = Convert.ToInt32(Console.ReadLine());
+            if (userNum3 >= 5)
+                Console.WriteLine("You have selected an index number that does not exist.");
+            Console.WriteLine(mixArray[userNum3] + " Press ENTER.");
+            Console.ReadLine();
+        }
+    }
+}
