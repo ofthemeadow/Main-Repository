4a6d466931249f13d57abbf246861ebd3b9cc6da
ofthemeadow <37228366+ofthemeadow@users.noreply.github.com>
2018-08-06 12:55:55 -0700

    overload?

---
+++ C#/MethodOverload/MethodOverload/DivideTwo.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodOverload
+{
+    class DivideTwo
+    {
+        //public static void Div(int num1, int num2)
+        //{
+        //    Console.WriteLine(num1 / num2);
+        //}
+        public static void Div(double num1, int num2)
+        {
+            Console.WriteLine(num1 / num2 );
+        }
+    }
+}
---
+++ C#/MethodOverload/MethodOverload/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodOverload
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Give me a number and I'll divide it by two.");
+            DivideTwo.Div(Convert.ToInt32(Console.ReadLine()), 2);
+            Console.Read();
+        }
+    }
+}
