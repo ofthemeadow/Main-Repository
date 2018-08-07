c3637c16a4bb9af37cf8043cd0d47072ad4138ee
ofthemeadow <37228366+ofthemeadow@users.noreply.github.com>
2018-08-07 15:54:28 -0700

    abstract class

---
+++ C#/AbstractClasses122/AbstractClasses122/Person.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AbstractClasses122
+{
+    public abstract class Person //abstract means nothing will change or needs to be manipulated, final
+    {
+        public string firstName { get; set; }
+        public string lastName { get; set; }
+
+        public void SayName()
+        {
+            Console.WriteLine("Name: " + firstName + lastName);
+        }
+    }
+}
---
+++ C#/AbstractClasses122/AbstractClasses122/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AbstractClasses122
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee fullname = new Employee();
+            fullname.firstName = "Sample ";
+            fullname.lastName = "Student ";
+            fullname.SayName();
+            Console.Read();
+        }
+    }
+}
