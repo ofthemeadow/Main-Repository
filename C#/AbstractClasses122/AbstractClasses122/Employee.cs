e2c18eb9241afbc257319b89023a2af5ad8ae5a1
ofthemeadow <37228366+ofthemeadow@users.noreply.github.com>
2018-08-09 14:13:58 -0700

    generics

--- C#/AbstractClasses122/AbstractClasses122/Employee.cs
+++ C#/AbstractClasses122/AbstractClasses122/Employee.cs
@@ -6,8 +6,14 @@ using System.Threading.Tasks;
 
 namespace AbstractClasses122
 {
-    public class Employee : Person, IQuittable
+    public class Employee<T> : Person, IQuittable
     {
+        public List<T> Things { get; set;}
+
+        public static void Id(int num1)
+        {
+            throw new NotImplementedException();
+        }
         public new void SayName()
         {
             base.SayName();
--- C#/Overload128.cs
+++ C#/Overload128.cs
@@ -12,13 +12,28 @@ namespace AbstractClasses122
 
         static void Main(string[] args)
         {
-            Employee fullname = new Employee();
-            IQuittable justQuitIt = new Employee();
+            IQuittable justQuitIt = new Employee<string>();
+
+            Employee<string> fullname = new Employee<string>();
             fullname.firstName = "Sample ";
             fullname.lastName = "Student ";
             fullname.SayName();
             fullname.Quit();
 
+            Employee<string> DoB = new Employee<string>();
+            DoB.Things.Add("12.27.86");
+            foreach (Employee<string> date in (dynamic)DoB)
+            {
+                Console.WriteLine("Birthdate is: " + date);
+            }
+
+            Employee<int> favNum = new Employee<int>();
+            favNum.Things.Add(7);
+            foreach (Employee<int> numeroUno in (dynamic)favNum)
+            {
+                Console.WriteLine("Favourite number is: " + numeroUno);
+            }
+            
             if (num1 == num1)
             {
                 Console.WriteLine("The two employees share the same Employee ID Number.");
