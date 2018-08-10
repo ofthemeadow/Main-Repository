69d17bbeb1349839a190457f6d71673c730ff3be
ofthemeadow <37228366+ofthemeadow@users.noreply.github.com>
2018-08-09 17:47:48 -0700

    enum

---
+++ C#/enum/enum/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace @enum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool successFlag = false;
+            while (successFlag == false)
+            {
+                try
+                {
+                    Console.WriteLine("What is the current day of the week?");
+                    var userDay = Console.ReadLine();
+                    DayOfWeek day = (DayOfWeek)Enum.Parse(typeof(DayOfWeek), userDay);
+                    Console.WriteLine("{0} is a good day", day.ToString());
+                    successFlag = true;
+                }
+                catch (Exception)
+                {
+                    Console.WriteLine("Please enter an actual day of the week.");
+                }
+            }
+            Console.ReadLine();
+        }
+        public enum DayOfWeek
+        {
+            Monday,
+            Tuesday,
+            Wednesday,
+            Thursday,
+            Friday,
+            Saturday,
+            Sunday
+        }
+    }
+}
