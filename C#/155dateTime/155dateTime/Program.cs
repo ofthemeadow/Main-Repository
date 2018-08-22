d1d5bf565bccce3f5da0ee06834088f9bcbe1132
ofthemeadow <37228366+ofthemeadow@users.noreply.github.com>
2018-08-22 15:59:54 -0700

    datetime

---
+++ C#/155dateTime/155dateTime/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _155dateTime
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DateTime currentDateTime = DateTime.Now;
+            Console.WriteLine(currentDateTime);
+            Console.WriteLine("Give me a number. \n\tI will give you the time it will be. \nX being the number of hours added to current time.");
+            DateTime newHourDateTime = currentDateTime.AddHours(Convert.ToDouble(Console.ReadLine()));
+            Console.WriteLine(newHourDateTime);
+            Console.Read();
+        }
+    }
+}
